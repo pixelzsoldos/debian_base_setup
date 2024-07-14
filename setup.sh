8fba9d0fb5a321cc50fcde6356f3f90c5ccfebe3
Janix <szabo.janos@allwin.hu>
2024-07-14 18:09:00 +0200

    add readme

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
-sudo apt install git curl vim zsh htop nvm kitty inkscape vlc obs-studio -y
+sudo apt install git curl vim zsh htop nvm kitty inkscape vlc obs-studio audiacity -y
 sudo snap install discord
